c16dd3a1e785a8ebebef96035831c5f29ebf9203
Santiago Martí Olbrich <samarti@uc.cl>
2015-12-28 12:32:53 -0300

    Update dclean.sh

--- dclean.sh
+++ dclean.sh
@@ -1,7 +1,7 @@
 cd /home/ubuntu/wemeep-deployment/meep-service
 docker-compose stop
 docker-compose rm -f
-docker rm -v $(docker ps -a -q -f status=exited)
-docker rmi $(docker images -f "dangling=true" -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes
 docker-compose up -d
