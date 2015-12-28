6153d66fc94c3912dad1fc9e098da192a944753b
Ubuntu <ubuntu@ip-172-31-26-6.sa-east-1.compute.internal>
2015-12-28 22:25:36 +0000

    Mejor

--- dclean.sh
+++ dclean.sh
@@ -1,7 +1,2 @@
 cd /home/ubuntu/wemeep-deployment/meep-service
-docker-compose stop
-docker-compose rm -f
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes
-docker-compose up -d
+bash dclean.sh
